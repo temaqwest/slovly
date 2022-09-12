7c52b8353ce791d5c15e497b178932889ae02a1e
temaqwest <taylorartem95@gmail.com>
2022-09-13 01:20:32 +0300

    Добавил сброс дефолтных стилей браузера, а также меню и анимацию для шапки (не адаптив)

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,30 @@
 <template>
   <header class="header">
     <div class="header__menu">
-      <AppButton variant="clear" class="header__burger">
+      <AppButton
+        variant="clear"
+        class="header__burger"
+        :class="{ 'header__burger--active': isMenuVisible }"
+        :aria-expanded="isMenuVisible"
+        aria-controls="header__list"
+        @click="toggleBurgerMenu"
+      >
         <div class="header__burger-line"></div>
         <div class="header__burger-line"></div>
         <div class="header__burger-line"></div>
       </AppButton>
-      <nav class="header__nav"></nav>
+      <nav
+        class="header__nav"
+        :class="{ 'header__nav--active': isMenuVisible }"
+      >
+        <ul class="header__list">
+          <li class="header__list-item">1</li>
+          <li class="header__list-item">2</li>
+          <li class="header__list-item">3</li>
+          <li class="header__list-item">4</li>
+          <li class="header__list-item">5</li>
+        </ul>
+      </nav>
     </div>
     <div class="header__logo">SLOVLY</div>
     <div class="header__utils">
@@ -27,13 +45,13 @@
 import AppIcon from '@/components/UI/AppIcon.vue'
 // import AppTooltip from '@/components/UI/AppTooltip.vue'
 import AppButton from '@/components/UI/AppButton.vue'
-// interface HeaderProps {
-//
-// }
-//
-// interface HeaderEmits {
-//
-// }
+import { ref } from 'vue'
+
+const isMenuVisible = ref<boolean>(false)
+
+function toggleBurgerMenu() {
+  isMenuVisible.value = !isMenuVisible.value
+}
 </script>
 
 <style lang="scss" scoped>
@@ -44,7 +62,7 @@ import AppButton from '@/components/UI/AppButton.vue'
   display: grid;
   grid-template-columns: 1fr 10fr 1fr;
   align-items: center;
-  min-height: 65px;
+  height: $header-height;
   padding: 0 20px;
   border-bottom: 1px solid $gray-color-1;
 
@@ -53,8 +71,25 @@ import AppButton from '@/components/UI/AppButton.vue'
     flex-flow: column nowrap;
     row-gap: 3px;
     width: min-content;
+    transition: all $transition-delay ease-in;
 
-    &:hover {
+    &--active {
+      transform: rotate(45deg);
+
+      & .header__burger-line:first-child {
+        transform: translateY(200%) rotate(-90deg);
+      }
+
+      & .header__burger-line:nth-child(3) {
+        opacity: 0;
+      }
+
+      & .header__burger-line:last-child {
+        transform: translateY(-200%) rotate(90deg);
+      }
+    }
+
+    &:hover:not(.header__burger--active) {
       .header__burger-line:first-child {
         transform: translateX(-3px);
       }
@@ -73,11 +108,44 @@ import AppButton from '@/components/UI/AppButton.vue'
     }
   }
 
-  &__nav {
+  &__menu {
+    position: relative;
+    justify-self: start;
   }
 
-  &__menu {
-    justify-self: start;
+  &__nav {
+    position: fixed;
+    top: $header-height;
+    bottom: 0;
+    left: -30%;
+    width: 30%;
+    background-color: $color-white;
+    box-shadow: 0 3px 6px rgb(0 0 0 / 16%), 0 3px 6px rgb(0 0 0 / 23%);
+    visibility: hidden;
+    opacity: 0;
+    transition: all $transition-delay-boring ease-out;
+
+    &--active {
+      visibility: visible;
+      opacity: 1;
+      left: 0;
+    }
+  }
+
+  &__list {
+    list-style: none;
+
+    &-item {
+      padding: 10px 15px;
+      width: 100%;
+      text-align: left;
+      transition: background-color $transition-delay ease-in;
+      cursor: pointer;
+
+      &:hover {
+        background-color: $gray-color-1;
+      }
+    }
   }
 
   &__logo {
